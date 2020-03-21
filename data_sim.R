f0f4fb0564cc6b418869c32dfd387bd3cbc22734
Florian Schmoll <florians@local.eoda.de>
2020-03-21 12:48:40 +0100

    Datenbank auf Basis von Supermarktdatensatz aktualisiert

--- data_sim.R
+++ data_sim.R
@@ -8,15 +8,20 @@ library(tsibble)
 
 
 # Kassel Stores -----------------------------------------------------------
-sm_df <- tibble(ID = 1:800, Name = if_else(is.na(cinema$osm_points$name), "", cinema$osm_points$name), 
-                Lon = st_coordinates(st_centroid(cinema$osm_points$geometry))[,1], 
-                Lat = st_coordinates(st_centroid(cinema$osm_points$geometry))[,2], City = "Kassel")
+sm_df <- read_delim("supermarkets.csv", delim = "\t") %>% filter(!is.na(geometry))
+
+sm_df <- tibble(ID = 1:nrow(sm_df), Name = if_else(is.na(sm_df$name), "", sm_df$name), 
+                Lon = unlist(str_extract_all(sm_df$geometry, "[0-9]+\\.[0-9]+") %>% lapply("[[",1)), 
+                Lat = unlist(str_extract_all(sm_df$geometry, "[0-9]+\\.[0-9]+") %>% lapply("[[",2)), 
+                City = "Kassel")
+
+
 
 con <- dbConnect(RSQLite::SQLite(), "storeTrackeDB.sqlite")
 
 dbListTables(con)
 
-dbWriteTable(con, "Supermarket", sm_df)
+dbWriteTable(con, "Supermarket", sm_df, overwrite = TRUE)
 dbListTables(con)
 
 res <- dbSendQuery(con, "SELECT * FROM Supermarket")
@@ -32,7 +37,7 @@ data(vic_elec)
 visitors <- tibble(Supermarket_ID = numeric(), Date = as.Date(character()), 
                    Hour = numeric(), Customers = numeric())
 
-for (i in 1:800){
+for (i in 1:nrow(sm_df)){
   print(i)
   samp_week <- yearweek(sample(seq.Date(as.Date("2012-01-01"), as.Date("2014-12-31"), by = "day"), 1))
   samp_max <- 100 + round(20 * runif(1, -1, 1))
@@ -85,7 +90,7 @@ dbFetch(res)
 
 # Product Capacity --------------------------------------------------------
 prod_cap <- expand.grid(Date = seq.POSIXt(as.POSIXct("2020-03-16"),as.POSIXct("2020-03-23"), by = "hour"),
-                        Supermarket_ID = 1:800,
+                        Supermarket_ID = 1:nrow(sm_df),
                         Product_ID = 1:4) %>% 
   as_tibble()
 
@@ -102,9 +107,5 @@ miss_ind <- sample(1:nrow(prod_cap), 800*4*14, replace = TRUE)
 dbWriteTable(con, "Stock",  prod_cap %>%
                mutate(Cap = if_else(row_number() %in% miss_ind, as.numeric(NA), Cap)) %>% 
                fill(Cap, .direction = "down"), overwrite = TRUE)
-
-dbClearResult(res)
-
-
 
 dbDisconnect(con)
