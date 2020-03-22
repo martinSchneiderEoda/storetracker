01a0e81648c65f05c4425e9ef4222d83e6af83de
Florian Schmoll <florians@local.eoda.de>
2020-03-22 12:03:29 +0100

    new store data

--- storetrackeR/server.R
+++ storetrackeR/server.R
@@ -165,7 +165,7 @@ shinyServer(function(input, output, session) {
                              c("ID" = "Supermarket_ID"))
          
       
-        print(markets$Score)
+        #print(markets$Score)
         
         getColor <- function(markets) {
             sapply(markets$Score, function(Score) {
